b24702e0e182c094e647e833c1e1849654a7ccea
yong.wang <yong.wang@ucarinc.com>
2019-02-20 05:55:56 +0800

    第二章第一个类示例

---
+++ ch02/demo01.py
@@ -0,0 +1,18 @@
+#!/bin/python
+# -*- coding:utf-8 -*-
+
+# 定义对象
+"""
+定义类注意的事项
+关键词是class 类名(其中类规范是首字母大写,单次规范是驼峰式)
+
+"""
+
+
+class MyFirstClass:
+    pass  # pass 关键词是小写的
+
+
+if __name__ == '__main__':
+    a = MyFirstClass()
+    print(a)
